ff49e869cc4522d2301c88c37825218f41229409
barremans <bmcbarre@gmail.com>
2023-10-22 16:05:14 +0200

    Update main.py
    Seperated into py files

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-# main.py
+import os
 import tkinter as tk
 from main_window import Main
 from version_info import __version__
 from version_info import __AppName__
+
+def set_window_icon(root):
+    icon_path = os.path.join(os.path.dirname(__file__), 'images', 'Graphicloads-Android-Settings-Contact.ico')
+    if os.path.exists(icon_path):
+        root.wm_iconbitmap(icon_path)
 
 def main():
     root = tk.Tk()
@@ -15,10 +20,11 @@ def main():
     y = (sh - h) / 2
     root.geometry('{0}x{1}+{2}+{3}'.format(w, h, int(x), int(y)))
     root.resizable(width=False, height=False)
-    root.wm_iconbitmap('/images/Graphicloads-Android-Settings-Contact.ico')
+
+    set_window_icon(root)  # Call the function to set the window icon dynamically
+
     Main(root)
     root.mainloop()
-
 
 if __name__ == '__main__':
     main()
