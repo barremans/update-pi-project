121cc700c027ad8101da28669adbb5b5282151c5
barremans <bmcbarre@gmail.com>
2023-10-22 17:25:49 +0200

    Update version_info.py
    change version

--- version_info.py
+++ version_info.py
@@ -1,7 +1,7 @@
 # version_info.py
 __author__ = 'The real barremans'
 __copyright__ = 'Copyright (C) 2023, Barre'
-__credits__ = ['Victor Santiago']
+__credits__ = ['bart']
 __license__ = 'GNU License'
 __version__ = '0.0.1'
 __maintainer__ = 'Barre'
